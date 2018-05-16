7a0037d7c51947f3c6c5800402c72afd953a4527
Slava Petrov <slavik--petrov@yandex.ru>
2018-05-16 11:54:02 +0000

    Modify restaurants page

--- app/admin/restaurants.rb
+++ app/admin/restaurants.rb
@@ -1,15 +1,32 @@
-ActiveAdmin.register Restaurant do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+# frozen_string_literal: true
 
+# register
+ActiveAdmin.register Restaurant do
+  index do
+    column :title
+    column :description
+    column :dishes_count do |product|
+      product.dishes.count
+    end
+    actions
+  end
+
+  show do |restaurant|
+    attributes_table do
+      row :title
+      row :description
+    end
+
+    panel 'Dishes' do
+      table_for restaurant.dishes do |t|
+        t.column :title
+        t.column :dish_type
+        t.column :ingredients
+        t.column :description
+        t.column :price
+      end
+    end
+  end
+
+  permit_params :title, :description
 end
